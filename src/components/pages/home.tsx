5ecf4717b7450fe238971f8617bf63249afd382f
Nathalia Rus <nathaliarus@gmail.com>
2021-04-02 23:10:11 +0100

    basic react app ts

---
+++ src/components/pages/home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+class NotFoundPage extends React.Component<Props, {}> {
+    render() {
+        return <div>home</div>
+    }
+}
+
+export default NotFoundPage
