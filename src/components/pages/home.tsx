ddd4b6cc8a603fae768b6a4f75518a861bed6e65
Nathalia Rus <nathaliarus@gmail.com>
2021-04-02 23:20:15 +0100

    router stuff

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-class NotFoundPage extends React.Component<Props, {}> {
+class Homepage extends React.Component<Props, {}> {
     render() {
         return <div>home</div>
     }
 }
 
-export default NotFoundPage
+export default Homepage
